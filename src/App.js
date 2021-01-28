5f89d5c721111fe0783adf5963b1922d7afc552d
Raheem Abolfathzadeh <72184069+RaheemAbol@users.noreply.github.com>
2021-01-27 21:08:33 -0500

    Update App.js
    Added functionality and comments

--- src/App.js
+++ src/App.js
@@ -1,43 +1,68 @@
-import {useState} from 'react';
+// importing useState and UseEffect \\
+// importing player from our components folder \\
+
+import {useState, useEffect} from 'react';
 import Player from './components/Player';
 
 
-function App() {
-  const [songs, setSongs] = useState([
+export default function App() {
+// using the state of useState to create an array [songs] which will be looped through \\
+  const [songs] = useState([
     {
-      title: "Look at me now",
+      title: "Falling down Ft. Lil Peep",
       artist: "XXXtentacion",
-      img_src: "./images/musicappimg1.jpg" 
+      img_src: "./images/musicappimg1.jpg", 
+      src: "./music/lilpeep.mp3"
     },
     {
-      title: "Song 2",
-      artist: "Artist 2",
-      img_src: "./images/musicappimg2.jpg"
+      title: "Saturday Night Special",
+      artist: "Lynard Skynard",
+      img_src: "./images/musicappimg2.jpg",
+      src: "./music/lynard.mp3"
     },
     {
-      title: "Song 3",
-      artist: "Artist 3",
-      img_src: "./images/musicappimg3.jpg"
+      title: " Singapur (Remix) ",
+      artist: "El Alfa",
+      img_src: "./images/musicappimg5.jpg",
+      src: "./music/singapur.mp3"
     },
     {
-      title: "Song 4",
-      artist: "Artist 4",
-      img_src: "./images/musicappimg4.jpg"
+      title: "Sleeping on the Blacktop",
+      artist: "Coltor Wall",
+      img_src: "./images/musicappimg4.jpg",
+      src: "./music/coltorwall.mp3"
     }
   ]);
-
+// currentSongIndex state is initilized at zero \\
+// nextSongIndex will always hold the state of currentSongIndex plus one  \\
 
   const[currentSongIndex, setCurrentSongIndex] = useState(0);
   const[nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
 
+// useEffect will only call when currentSongIndex is updated. If not it will update on any change. \\
+// in lines 47-49 if currentSongIndex or our next song is greater than songs.length-1 or our array return to the start \\
+// in lines 50-51  if currentSongIndex is not greater than songs.length-1 than we will return the current song \\
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length -1) {
+        return 0;
+      }else{
+        return currentSongIndex + 1;
+      }
+    })
+  }, [currentSongIndex]);
+
+
+  // passing currentSongIndex, setCurrentSongIndex, nextSongIndex as props. Songs is our array \\
   return (
     <div className= "App">
-      <Player
-       song={songs[currentSongIndex]}
-       nextSong={songs[nextSongIndex]}
+    <Player
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
      />
     </div>
   )
 }
-
-export default App;
